ab810a967976fc32a6619c768a6711ef545bcf75
Auric Vente <mprops@protonmail.com>
2026-03-19 14:42:57 -0600

    Mods

--- src/status.py
+++ src/status.py
@@ -40,6 +40,9 @@ class Status:
             self.player.show_name_message("Tuning...")
             return
 
+        if not hasattr(self.player, "players") or not hasattr(self.player, "active_idx"):
+            return
+
         player_mpv = self.player.players[self.player.active_idx]
 
         if player_mpv and getattr(player_mpv, "playback_time", None) is not None:
