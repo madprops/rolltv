59dbcf62474157f2fce561946bf0198dfb29d1d6
Auric Vente <mprops@protonmail.com>
2026-03-19 19:06:01 -0600

    Mods

--- src/args.py
+++ src/args.py
@@ -16,16 +16,31 @@ class Args:
         default_sound_fx = saved_data.get("sound_fx", True)
 
         parser.add_argument(
-            "--no-status", action="store_false", dest="show_status", default=default_show_status
+            "--no-status",
+            action="store_false",
+            dest="show_status",
+            default=default_show_status,
+            help="Disable the status bar",
         )
 
         parser.add_argument(
-            "--no-sound-fx", action="store_false", dest="sound_fx", default=default_sound_fx
+            "--no-sound-fx",
+            action="store_false",
+            dest="sound_fx",
+            default=default_sound_fx,
+            help="Disable sound effects",
+        )
+
+        parser.add_argument(
+            "--captures",
+            type=str,
+            help="The path for the video captures",
         )
 
         parsed_args = parser.parse_args()
         self.show_status = parsed_args.show_status
         self.sound_fx = parsed_args.sound_fx
+        self.captures = parsed_args.captures
 
 
 args = Args()
